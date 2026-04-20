5b770c611a6c95426caae64e45ddae0d7aca3176
Ananya <ananyaaol07@gmail.com>
2026-04-20 09:30:28 +0530

    Initial TFT Project Upload

---
+++ DataBase/fee_system.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE fee_system;
+USE fee_system;
+
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    phone VARCHAR(15),
+    course VARCHAR(50),
+    total_fee INT
+);
+
+CREATE TABLE payments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    student_id INT,
+    amount INT,
+    payment_mode VARCHAR(20),
+    transaction_id VARCHAR(100),
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (student_id) REFERENCES students(id)
+);
+
+CREATE TABLE teachers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    t_name VARCHAR(100),
+    mob_no VARCHAR(10),
+    email VARCHAR(100),
+    due_date INT
+);
+
+CREATE TABLE users (
+ id INT AUTO_INCREMENT PRIMARY KEY,
+ fullname VARCHAR(100),
+ phone VARCHAR(20),
+ email VARCHAR(100),
+ password VARCHAR(100)
+);
+
+desc students;
+
+show tables;
+
+select * from teachers;
